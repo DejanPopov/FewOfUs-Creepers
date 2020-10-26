2f714f2f859aa23b2c79ac03ab64c92f4eb03fdd
Dejan Popov <dekipopica92@gmail.com>
2020-10-26 13:11:30 +0100

    Custom Editor script
    - Script that will be editor for type of AIWaypointNetwork script

---
+++ Assets/FewOfUs/Editor/AIWaypointNetworkEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Editor class
+using UnityEditor;
+
+//Editor class thats type of script that has list of waypoints
+[CustomEditor(typeof(AIWaypointNetwork))]
+public class AIWaypointNetworkEditor : Editor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
