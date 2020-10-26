83be226baee6cd782754ca2430a748fb75d63087
Dejan Popov <dekipopica92@gmail.com>
2020-10-26 17:05:49 +0100

    Create NavAgentExample.cs
    - Making script for Agent

---
+++ Assets/Navigation Example/NavAgentExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentExample : MonoBehaviour
+{
+    private NavMeshAgent navAgent = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //NavMesh agent reference
+        navAgent = GetComponent<NavMeshAgent>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
