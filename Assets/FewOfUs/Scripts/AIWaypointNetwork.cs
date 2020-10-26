dfb7740fbf32eb1b0819bb09f818ce077747c5ab
Dejan Popov <dekipopica92@gmail.com>
2020-10-26 14:44:37 +0100

    Update AIWaypointNetwork.cs
    - Made enumerations of options to be choosen that paths of waypoints are gone be visible or to see connections or to see actual paths for an AI

--- Assets/FewOfUs/Scripts/AIWaypointNetwork.cs
+++ Assets/FewOfUs/Scripts/AIWaypointNetwork.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PathDisplayMode
+{
+    None,
+    Connections,
+    Paths
+}
+
 public class AIWaypointNetwork : MonoBehaviour
 {
+    public PathDisplayMode DisplayMode = PathDisplayMode.Connections;
+
+
+
     public List<Transform> waypoints = new List<Transform>();
 
 }
