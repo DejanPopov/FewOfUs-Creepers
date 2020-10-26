9531fe13cfe8854c889c1f292cda790506219261
Dejan Popov <dekipopica92@gmail.com>
2020-10-26 17:19:45 +0100

    Update NavAgentExample.cs
    - Agent now goes to waypoint 0

--- Assets/Navigation Example/NavAgentExample.cs
+++ Assets/Navigation Example/NavAgentExample.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class NavAgentExample : MonoBehaviour
 {
+    //Option that we can assign Waypoint Network
+    public AIWaypointNetwork WaypointNetwork = null;
+
+    //Waypoint number
+    public int CurrentIndex = 0;
+
     private NavMeshAgent navAgent = null;
 
     // Start is called before the first frame update
     void Start()
     {
         //NavMesh agent reference
-        navAgent = GetComponent<NavMeshAgent>();   
+        navAgent = GetComponent<NavMeshAgent>();
+
+        if (WaypointNetwork == null)
+        {
+            return;
+        }
+
+        if (WaypointNetwork.waypoints[CurrentIndex] != null)
+        {
+            navAgent.destination = WaypointNetwork.waypoints[CurrentIndex].position;
+        }
     }
 
     // Update is called once per frame
