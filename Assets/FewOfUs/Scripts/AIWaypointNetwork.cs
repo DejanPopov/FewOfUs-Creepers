df2ee63f30fec5c24be2a425ee2dc48d9a701f6d
Dejan Popov <dekipopica92@gmail.com>
2020-10-26 11:41:35 +0100

    Deleted standard assets
    - Tried first build

---
+++ Assets/FewOfUs/Scripts/AIWaypointNetwork.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWaypointNetwork : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+}
