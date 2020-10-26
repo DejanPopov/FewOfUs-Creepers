6779f134f12a9f06ff2b86c414e3ed18f4bacb2e
Dejan Popov <dekipopica92@gmail.com>
2020-10-26 16:15:03 +0100

    Update AIWaypointNetwork.cs
    - Hiding these options in inspector

--- Assets/FewOfUs/Scripts/AIWaypointNetwork.cs
+++ Assets/FewOfUs/Scripts/AIWaypointNetwork.cs
@@ -11,10 +11,13 @@ public enum PathDisplayMode
 
 public class AIWaypointNetwork : MonoBehaviour
 {
+    //Hiding this in Unity inspector
+    [HideInInspector]
     public PathDisplayMode DisplayMode = PathDisplayMode.Connections;
-
     //For waypoints
+    [HideInInspector]
     public int UIStart = 0;
+    [HideInInspector]
     public int UIEnd = 0;
 
 
