4360bcc5b363c1989790eaa1d62cc54662aeaa66
Dejan Popov <dekipopica92@gmail.com>
2020-10-27 20:56:44 +0100

    Create SlidingDoorDemo.cs
    - Script that will make door open and close

---
+++ Assets/Navigation Example/SlidingDoorDemo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Door states enumerator
+public enum DoorState
+{
+    Open,
+    Animating,
+    Closed
+}
+
+public class SlidingDoorDemo : MonoBehaviour
+{
+    public float SlidingDistance = 4.0f;
+    public float Duration = 1.5f;
+    public AnimationCurve JumpCurve = new AnimationCurve();
+
+    private Transform transform = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
