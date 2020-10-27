252654f1517fdeb6fa3ee378b12170278f4c302c
Dejan Popov <dekipopica92@gmail.com>
2020-10-27 21:17:53 +0100

    Update SlidingDoorDemo.cs
    - Conflict to resolve

--- Assets/Navigation Example/SlidingDoorDemo.cs
+++ Assets/Navigation Example/SlidingDoorDemo.cs
@@ -16,18 +16,49 @@ public class SlidingDoorDemo : MonoBehaviour
     public float Duration = 1.5f;
     public AnimationCurve JumpCurve = new AnimationCurve();
 
-    private Transform transform = null;
+    //Door states
+    private Transform transformT = null;
+    private Vector3 openPos = Vector3.zero;
+    private Vector3 closedPos = Vector3.zero;
+    private DoorState doorState = DoorState.Closed;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transformT = transform;
+        closedPos = transformT.position;
+        //Vector that will be pointing right (open door to that side)
+        openPos = closedPos + (transform.right * SlidingDistance);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Door will open with Space bar key
+        if (Input.GetKeyDown(KeyCode.Space) && doorState != DoorState.Animating)
+        {
+            StartCoroutine(AnimateDoor(doorState == DoorState.Open) ? DoorState.Closed : DoorState.Open);
+        }
+    }
+
+    IEnumerator AnimateDoor(DoorState newState)
+    {
+        doorState = DoorState.Animating;
+        float time = 0.0f;
+        //Star and end position of interpolation
+        Vector3 startPos = (newState == DoorState.Open) ? closedPos : openPos;
+        Vector3 endPos = (newState == DoorState.Open) ? openPos : closedPos;
+
+        while (time <= Duration)
+        {
+            float t = time / Duration;
+            transformT.position = Vector3.Lerp(startPos, endPos, JumpCurve.Evaluate(t));
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transformT.position = endPos;
+        doorState = newState;
     }
 }
