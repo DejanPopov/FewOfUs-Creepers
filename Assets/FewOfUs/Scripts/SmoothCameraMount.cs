369a31435d1d67e49f7f0d68f96eb0669999e14f
Dejan Popov <dekipopica92@gmail.com>
2020-10-29 13:57:03 +0100

    Update SmoothCameraMount.cs
    - Camera following zombie

--- Assets/FewOfUs/Scripts/SmoothCameraMount.cs
+++ Assets/FewOfUs/Scripts/SmoothCameraMount.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SmoothCameraMount : MonoBehaviour
 {
+    public Transform Mount = null;
+    //Camera speed
+    public float Speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,10 @@ public class SmoothCameraMount : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        //Find position of the camera
+        transform.position = Vector3.Lerp(transform.position, Mount.position, Time.deltaTime * Speed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Mount.rotation, Time.deltaTime * Speed);
     }
 }
