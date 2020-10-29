4a802aa981f46bac4891d74327e5f50c05db0ac7
Dejan Popov <dekipopica92@gmail.com>
2020-10-29 14:29:33 +0100

    Update Controller.cs

--- Assets/FewOfUs/Scripts/Controller.cs
+++ Assets/FewOfUs/Scripts/Controller.cs
@@ -6,10 +6,15 @@ public class Controller : MonoBehaviour
 {
 
     private Animator animator = null;
+    private int horizontalHash = 0;
+    private int verticalHash = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        horizontalHash = Animator.StringToHash("Horizontal");
+        verticalHash = Animator.StringToHash("Vertical");
     }
 
     // Update is called once per frame
@@ -20,7 +25,7 @@ public class Controller : MonoBehaviour
         float yAxis = Input.GetAxis("Vertical") * 5.66f;
 
         //Seting values from inputs
-        animator.SetFloat("Horizontal", xAxis, 1.0f, Time.deltaTime);
-        animator.SetFloat("Vertical", yAxis, 1.0f, Time.deltaTime);
+        animator.SetFloat(horizontalHash, xAxis, 1.0f, Time.deltaTime);
+        animator.SetFloat(verticalHash, yAxis, 1.0f, Time.deltaTime);
     }
 }
