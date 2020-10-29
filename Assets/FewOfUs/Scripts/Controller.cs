b7f9bc77dbe0f406aa2699525c478bb468868fce
Dejan Popov <dekipopica92@gmail.com>
2020-10-29 14:23:59 +0100

    Update Controller.cs
    - Simple controller for input parameters

--- Assets/FewOfUs/Scripts/Controller.cs
+++ Assets/FewOfUs/Scripts/Controller.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+
+    private Animator animator = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Multiply for values are taken from Locomotion blend
+        float xAxis = Input.GetAxis("Horizontal") * 2.32f;
+        float yAxis = Input.GetAxis("Vertical") * 5.66f;
+
+        //Seting values from inputs
+        animator.SetFloat("Horizontal", xAxis);
+        animator.SetFloat("Vertical", yAxis);
     }
 }
