b7f6e2d38223073d694d971cad133bc536557be6
Dejan Popov <dekipopica92@gmail.com>
2020-10-30 19:05:29 +0100

    Update AIState.cs

--- Assets/FewOfUs/Scripts/AI/AIState.cs
+++ Assets/FewOfUs/Scripts/AI/AIState.cs
@@ -11,4 +11,40 @@ public abstract class AIState : MonoBehaviour
     {
         machineM = stateMachine;
     }
+
+    //Default handlers
+    public virtual void OnEnterState()
+    {
+
+    }
+
+    public virtual void OnExitState()
+    {
+
+    }
+
+    public virtual void OnUpdate()
+    {
+
+    }
+
+    public virtual void OnAnimatorUpdated()
+    {
+
+    }
+
+    public virtual void OnAnimatorIKUpdated()
+    {
+
+    }
+
+    public virtual void OnTriggerEvent(AITriggerEventType eventType, Collider other)
+    {
+
+    }
+
+    public virtual void OnDestinationReached(bool isReached)
+    {
+
+    }
 }
