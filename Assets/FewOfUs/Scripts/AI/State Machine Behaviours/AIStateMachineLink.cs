37e5cd781f7ba507c55505793ba2265170b8c9a0
Dejan Popov <dekipopica92@gmail.com>
2020-10-31 13:16:50 +0100

    Create AIStateMachineLink.cs

---
+++ Assets/FewOfUs/Scripts/AI/State Machine Behaviours/AIStateMachineLink.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIStateMachineLink : StateMachineBehaviour
+{
+    private AIStateMachine stateMachineS;
+    public AIStateMachine stateMachine { set { stateMachineS = value; } }
+}
