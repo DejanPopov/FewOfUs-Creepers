f3bb0c9a31207b8abb75cb7433cdca0ddee6cddc
Dejan Popov <dekipopica92@gmail.com>
2020-10-31 12:10:12 +0100

    Update AISensor.cs

--- Assets/FewOfUs/Scripts/AI/AISensor.cs
+++ Assets/FewOfUs/Scripts/AI/AISensor.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AISensor : MonoBehaviour
 {
+    private AIStateMachine parentStateMachine = null;
+    public AIStateMachine parentStateMachines { set { parentStateMachine = value; } }
+
     // Start is called before the first frame update
     void Start()
     {
