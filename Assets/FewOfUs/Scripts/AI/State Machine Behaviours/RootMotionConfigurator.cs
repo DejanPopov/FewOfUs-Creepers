57eb77dffde893ad4e74b7c1cfea10e74f1d7dd2
Dejan Popov <dekipopica92@gmail.com>
2020-10-31 13:26:13 +0100

    Create RootMotionConfigurator.cs

---
+++ Assets/FewOfUs/Scripts/AI/State Machine Behaviours/RootMotionConfigurator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class RootMotionConfigurator : AIStateMachineLink
+{
+    [SerializeField]
+    private int rootPosition = 0;
+
+    [SerializeField]
+    private int rootRotation = 0;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateMachineS)
+        {
+            stateMachineS.AddRootMotionRequest(rootPosition, rootRotation);
+        }
+       // base.OnStateEnter(animator, stateInfo, layerIndex);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateMachineS)
+        {
+            stateMachineS.AddRootMotionRequest(- rootPosition, - rootRotation);
+        }
+
+        //base.OnStateExit(animator, stateInfo, layerIndex);
+    }
+}
