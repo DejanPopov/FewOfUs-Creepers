3976c805a14a68bcfe1a33f5240dec6c8d03ccd7
Dejan Popov <dekipopica92@gmail.com>
2021-05-22 18:46:49 +0200

    Update AIZombieState_Idle1.cs
    Random states

--- Assets/FewOfUs/Scripts/AI/AIZombieState_Idle1.cs
+++ Assets/FewOfUs/Scripts/AI/AIZombieState_Idle1.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class AIZombieState_Idle1 : AIZombieState
 {
+    //Random zombie choosing state
+    [SerializeField] Vector2 idleTimeRange = new Vector2(10.0f, 60.0f);
+
+    private float idleTime = 0.0f;
+    private float timer = 0.0f;
     public override AIStateType OnUpdate()
     {
-        Debug.Log("State type beeing fetched by state machine");
+
         return AIStateType.Idle;
+    }
+    public override void OnEnterState()
+    {
+        Debug.Log("Entering idle state");
+        base.OnEnterState();
+        if ( zombieStateMachine == null)
+        {
+            return;
+        }
+
+        idleTime = Random.Range(idleTimeRange.x, idleTimeRange.y);
+        timer = 0.0f;
+
+        zombieStateMachine.NavAgentControll(true, false);
+        zombieStateMachine.speedS = 0;
+        zombieStateMachine.seekingS = 0;
+        zombieStateMachine.feddingF = false;
+
     }
     public override AIStateType getStateType()
     {
