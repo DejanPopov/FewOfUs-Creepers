3d003f7ce29baed2a6168e3d29ba9163b8d167da
Dejan Popov <dekipopica92@gmail.com>
2021-05-22 16:31:39 +0200

    Update AIState.cs
    Helper function to calculate world space postion of sphere center and radius

--- Assets/FewOfUs/Scripts/AI/AIState.cs
+++ Assets/FewOfUs/Scripts/AI/AIState.cs
@@ -39,6 +39,28 @@ public abstract class AIState : MonoBehaviour
         }
     }
 
+    public static void ConvertSphereColliderToWorldSpace (SphereCollider col, out Vector3 pos, out float radius)
+    {
+        pos = Vector3.zero;
+        radius = 0.0f;
+
+        if (col == null)
+        {
+            return;
+        }
+        //Calcualte world space position of sphere center
+        pos = col.transform.position;
+        pos.x += col.center.x * col.transform.lossyScale.x;
+        pos.y += col.center.y * col.transform.lossyScale.y;
+        pos.z += col.center.z * col.transform.lossyScale.z;
+
+        // Calculate world space radius of sphere
+        radius = Mathf.Max(col.radius * col.transform.lossyScale.x,
+                col.radius * col.transform.lossyScale.y);
+
+        Mathf.Max(radius, col.radius * col.transform.lossyScale.z);
+    }
+
     public virtual void OnAnimatorIKUpdated()
     {
 
