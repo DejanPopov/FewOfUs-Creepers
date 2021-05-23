d1a328f3af66af5758e493419ee0a120c37dced4
Dejan Popov <dekipopica92@gmail.com>
2021-05-23 16:45:01 +0200

    Update AIZombieState_Patrol1.cs
    Logic for partol state

--- Assets/FewOfUs/Scripts/AI/AIZombieState_Patrol1.cs
+++ Assets/FewOfUs/Scripts/AI/AIZombieState_Patrol1.cs
@@ -6,12 +6,54 @@ public class AIZombieState_Patrol1 : AIZombieState
 {
     //Inspector assigned
     [SerializeField] AIWaypointNetwork waypoinyNetwork = null;
-    [SerializeField] bool randomCOntrol = false;
+    [SerializeField] bool randomPatrol = false;
     [SerializeField] int currentWaypoint = 0;
     [SerializeField] [Range(0.0f, 3.0f)] float speed = 1.0f;
+
     public override AIStateType getStateType()
     {
         return AIStateType.Partol;
+    }
+
+    public override void OnEnterState()
+    {
+        Debug.Log("Entering partol state");
+        base.OnEnterState();
+        if (zombieStateMachine == null)
+        {
+            return;
+        }
+
+        zombieStateMachine.NavAgentControll(true, false);
+        zombieStateMachine.speedS = speed;
+        zombieStateMachine.seekingS = 0;
+        zombieStateMachine.feddingF = false;
+        zombieStateMachine.attackTypeA = 0;
+
+        if (zombieStateMachine.targetType != AITargetType.Waypoint)
+        {
+            zombieStateMachine.ClearTarget();
+
+            if (waypoinyNetwork != null && waypoinyNetwork.waypoints.Count > 0)
+            {
+                if (randomPatrol)
+                {
+                    currentWaypoint = Random.Range(0, waypoinyNetwork.waypoints.Count - 1);
+                }
+
+                Transform waypoint = waypoinyNetwork.waypoints[currentWaypoint];
+                if (waypoint != null)
+                {
+                    zombieStateMachine.SetTarget(AITargetType.Waypoint, null, waypoint.position,
+                        Vector3.Distance(zombieStateMachine.transform.position, waypoint.position));
+                }
+
+                zombieStateMachine.navAgentN.SetDestination(waypoint.position);
+                
+            }
+        }
+
+        zombieStateMachine.navAgentN.Resume();
     }
 
     public override AIStateType OnUpdate()
