9041dab51870d7028ccc4e41d138dcd014ebe578
Dejan Popov <dekipopica92@gmail.com>
2021-05-23 15:50:05 +0200

    Update AIZombieState_Patrol1.cs
    Added variables for inspector

--- Assets/FewOfUs/Scripts/AI/AIZombieState_Patrol1.cs
+++ Assets/FewOfUs/Scripts/AI/AIZombieState_Patrol1.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class AIZombieState_Patrol1 : AIZombieState
 {
+    //Inspector assigned
+    [SerializeField] AIWaypointNetwork waypoinyNetwork = null;
+    [SerializeField] bool randomCOntrol = false;
+    [SerializeField] int currentWaypoint = 0;
+    [SerializeField] [Range(0.0f, 3.0f)] float speed = 1.0f;
     public override AIStateType getStateType()
     {
         return AIStateType.Partol;
